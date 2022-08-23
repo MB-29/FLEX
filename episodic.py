8f1d5fd14e8a3296af606038d12736f83fc1391a
MB-29 <55185203+MB-29@users.noreply.github.com>
2022-08-23 16:52:28 +0200

    single agent

--- episodic.py
+++ episodic.py
@@ -22,7 +22,7 @@ task_dt = 80e-3
 
 T = 100
 T_task = 100
-n_samples = 1
+n_samples = 5
 T_random = 0
 n_episodes = 20
 n_gradient = 400
@@ -34,78 +34,76 @@ sigma = environment.sigma
 
 x0 = environment.x0
 
-# for agent_ in [Random, Active]:
+# for Agent in [Random, Active]:
 agents = {
     # 'passive':{'agent': Passive, 'color': 'black'},
-    'D-optimal': {'agent': Linearized, 'color': 'blue'},
-    'random': {'agent': Random, 'color': 'red'},
+    'D-optimal': Linearized}
+    # 'random': {'agent': Random, 'color': 'red'},
     # 'uniform': {'agent': Spacing, 'color': 'green'},
     # # 'gradientOD': {'agent': GradientDesign, 'color': 'purple'},
     # # 'variation': {'agent': Variation, 'color': 'color'},
-}
 output = {}
-
+name = 'D-optimal'
+Agent = agents[name]
 fig, (ax1, ax2) = plt.subplots(2, 1)
 if __name__ == '__main__':
     task_id = int(sys.argv[1])
-    for name, value in agents.items():
-        print(f'agent {name}')
-        estimation_values = np.zeros((n_samples, n_episodes*T))
-        exploitation_values = np.zeros((n_samples, n_episodes))
-        output[name] = {'estimation': estimation_values, 'exploitation':exploitation_values}
-        for sample_index in tqdm(range(n_samples)):
-            agent_ = value['agent']
-            color = value['color']
-            NeuralModel
-            # print(f'Model = {Model}')
-        # for agent_ in [Spacing]:
-            model = NeuralModel()
-            # print('exploration')
-            for episode in range(n_episodes):
-                print(f'episode {episode}')
-                agent = agent_(
-                    x0.copy(),
-                    environment.m,
-                    environment.dynamics,
-                    model,
-                    gamma,
-                    dt
-                )
-                estimation_error = agent.identify(
-                    T,
-                    test_function=environment.test_error,
-                    T_random=0
-                )
-                # output[name] = estimation_values
-                estimation_values[sample_index, episode*T:(episode+1)*T] = estimation_error
+    print(f'agent {name}')
+    estimation_values = np.zeros((n_samples, n_episodes*T))
+    exploitation_values = np.zeros((n_samples, n_episodes))
+    output[name] = {'estimation': estimation_values, 'exploitation':exploitation_values}
+    for sample_index in tqdm(range(n_samples)):
+        NeuralModel
+        # print(f'Model = {Model}')
+    # for Agent in [Spacing]:
+        model = NeuralModel()
+        # print('exploration')
+        for episode in range(n_episodes):
+            print(f'episode {episode}')
+            agent = Agent(
+                x0.copy(),
+                environment.m,
+                environment.dynamics,
+                model,
+                gamma,
+                dt
+            )
 
-                # print('exploitation')
-                cost, loss_values = exploitation(
-                    exploitation_environment,
-                    model.model,
-                    T_task,
-                    n_gradient=n_gradient
-                    )
-                exploitation_values[sample_index, episode] = cost
-        output[name]['estimation'] = estimation_values
-        output[name]['exploitation'] = exploitation_values
-        exploitation_mean = exploitation_values.mean(axis=0)
-        exploitation_std = np.sqrt(exploitation_values.var(axis=0)/n_samples)
-        # ax1.plot(exploitation_mean, label=name)
-        # ax1.fill_between(
-        #     np.arange(n_episodes),
-        #     exploitation_mean-exploitation_std,
-        #     exploitation_mean+exploitation_std,
-        #     alpha=0.5)
-        # estimation_mean = estimation_values.mean(axis=0)
-        # estimation_std = np.sqrt(estimation_values.var(axis=0)/n_samples)
-        # ax2.plot(estimation_mean, label=name)
-        # ax2.fill_between(
-        #     np.arange(n_episodes*T),
-        #     estimation_mean-estimation_std,
-        #     estimation_mean+estimation_std,
-        #     alpha=0.5)
-OUTPUT_PATH = f'output/{ENVIRONMENT_NAME}_episodic-{task_id}.pkl'
+            cost, loss_values = exploitation(
+                exploitation_environment,
+                model.model,
+                T_task,
+                n_gradient=n_gradient
+                )
+            estimation_error = agent.identify(
+                T,
+                test_function=environment.test_error,
+                T_random=0
+            )
+            # output[name] = estimation_values
+            estimation_values[sample_index, episode*T:(episode+1)*T] = estimation_error
+
+            # print('exploitation')
+            exploitation_values[sample_index, episode] = cost
+    output[name]['estimation'] = estimation_values
+    output[name]['exploitation'] = exploitation_values
+    exploitation_mean = exploitation_values.mean(axis=0)
+    exploitation_std = np.sqrt(exploitation_values.var(axis=0)/n_samples)
+    # ax1.plot(exploitation_mean, label=name)
+    # ax1.fill_between(
+    #     np.arange(n_episodes),
+    #     exploitation_mean-exploitation_std,
+    #     exploitation_mean+exploitation_std,
+    #     alpha=0.5)
+    # estimation_mean = estimation_values.mean(axis=0)
+    # estimation_std = np.sqrt(estimation_values.var(axis=0)/n_samples)
+    # ax2.plot(estimation_mean, label=name)
+    # ax2.fill_between(
+    #     np.arange(n_episodes*T),
+    #     estimation_mean-estimation_std,
+    #     estimation_mean+estimation_std,
+    #     alpha=0.5)
+OUTPUT_PATH = f'output/{ENVIRONMENT_NAME}_{name}_episodic-{task_id}.pkl'
 with open(OUTPUT_PATH, 'wb') as output_file:
     pickle.dump(output, output_file)
 plt.legend()
